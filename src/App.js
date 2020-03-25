b20eb207480d993456b09915a093e1e715b2ee4f
German Rumm <german.rumm@gmail.com>
2020-03-25 22:51:29 +0100

    Update viewport information on resize and orientation change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import './App.css';
 
-function App() {
+function getDeviceInfo() {
   const devicePixelRatio = window.devicePixelRatio;
   const viewportWidth = Math.max(
     document.documentElement.clientWidth,
@@ -13,11 +13,27 @@ function App() {
     window.innerHeight || 0
   );
 
+  return { devicePixelRatio, viewportWidth, viewportHeight }
+}
+
+function App() {
+  const initialState = getDeviceInfo();
+  const [dimensions, setDimensions] = React.useState(initialState);
+  React.useEffect(() => {
+    function handleViewChange() {
+      const dimensions = getDeviceInfo();
+      setDimensions(dimensions);
+    }
+
+    window.addEventListener('orientationchange', handleViewChange);
+    window.addEventListener('resize', handleViewChange);
+  })
+
   return (
     <div className="App" id="content">
-      Device Pixel Ratio: {devicePixelRatio}<br />
-      Viewport width: {viewportWidth}<br />
-      Viewport height: {viewportHeight}<br />
+      Device Pixel Ratio: {dimensions.devicePixelRatio}<br />
+      Viewport width: {dimensions.viewportWidth}<br />
+      Viewport height: {dimensions.viewportHeight}<br />
 
       <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
     </div>
